358977d0881f7b05a5ec29dcc828117224df52ea
PeteTimesSix <Pete1001@seznam.cz>
2022-07-15 17:09:30 +0200

    even acidifier
    Now also acidifies whatever random nonsense the pawn had in their pockets.
    Also significantly weakens the skeleton (read: sets its hit points to a much lower value, resulting in skeletons that quickly deteriorate to nothing when exposed to the outdoors.)
    Also ensured that the torso actually suffers chemical burns, what with actually containing the death acidifier.

---
+++ DeathAcidifiest/Source/HarmonyPatches/DissolveGearOnDeath_Patches.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace PeteTimesSix.DeathAcidifiest.HarmonyPatches
+{
+    [HarmonyPatch(typeof(HediffComp_DissolveGearOnDeath),nameof(HediffComp_DissolveGearOnDeath.Notify_PawnDied))]
+    public static class HediffComp_DissolveGearOnDeath_Notify_PawnDied_Patches
+    {
+        [HarmonyPostfix]
+        public static void Postfix(HediffComp_DissolveGearOnDeath __instance)
+        {
+            var corpse = __instance?.Pawn?.Corpse;
+            if(corpse != null) 
+            {
+                CompRottable compRottable = corpse.TryGetComp<CompRottable>();
+                if(compRottable != null) 
+                {
+                    compRottable.RotProgress = compRottable.PropsRot.TicksToDessicated;
+                }
+                Rand.PushState(__instance.Pawn.thingIDNumber ^ 6457422); //made that number up
+                var min = Math.Max(1, corpse.MaxHitPoints / 50);
+                var max = Math.Max(1, corpse.MaxHitPoints / 5);
+                corpse.HitPoints = Rand.RangeInclusiveSeeded(min, max, __instance.Pawn.thingIDNumber ^ 6457422);
+                Rand.PopState();
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(HediffComp_DissolveGearOnDeath), nameof(HediffComp_DissolveGearOnDeath.Notify_PawnKilled))]
+    public static class HediffComp_DissolveGearOnDeath_Notify_PawnKilled_Patches
+    {
+        [HarmonyPostfix]
+        public static void Postfix(HediffComp_DissolveGearOnDeath __instance)
+        {
+            var pawn = __instance?.Pawn;
+            if(pawn != null)
+            {
+                pawn.inventory.DestroyAll(DestroyMode.Vanish);
+            }
+            var injuryDef = __instance.Props.injuryCreatedOnDeath;
+            var bodyPart = __instance.parent.Part;
+            if(injuryDef != null && bodyPart != null && !pawn.health.hediffSet.hediffs.Any(h => h.Part == bodyPart && h.def == injuryDef))
+                pawn.health.AddHediff(injuryDef, bodyPart);
+        }
+    }
+}
